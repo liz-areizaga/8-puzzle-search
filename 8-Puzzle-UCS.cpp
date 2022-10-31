d5ca77a4802b1eeeab0dd2de14a4132c005e40cd
Lizbeth Areizaga <lizbethareizaga@Lizbeths-MacBook-Air.local>
2022-10-31 10:56:12 -0700

    Modify operation functins

--- 8-Puzzle-UCS.cpp
+++ 8-Puzzle-UCS.cpp
@@ -15,25 +15,20 @@ Positions:
 
 struct problem {
   int **currState; // current state
-  int initialState[3][3] = {8,3,5,4,1,0,2,7,6}; //random start state
-  int goalState[3][3] = {1,2,3,4,5,6,7,8,0};
-  int x_pos = 1;
-  int y_pos = 2;
+  int x_pos; //x-Position for blank spot
+  int y_pos; //y-Position for blank spot
   int f_n = 0;// heuristic cost
   int g_n = 0;//depth cost
 
 };
 
 const int arrSize = 3;
+int goalState[3][3] = {1,2,3,4,5,6,7,8,0};
 
-
-//queue<int> q; //look up!
-//ask Prof about how exact he wants us to match the pseudo code
-//goal test is not a member of the problem, does it need to be?
-bool goalTest(problem currProb){
+bool equalProblems(problem prob1, problem prob2){ //returns true if given puzzles are the same
   for(unsigned int i = 0; i < arrSize; ++i){
     for(unsigned int j = 0; j < arrSize; ++j){
-      if(currProb.goalState[i][j] != currProb.currState[i][j]){ //if any cell does not match then goal state isn't reached
+      if(prob1.currState[i][j] != prob2.currState[i][j]){ //if any cell does not match then goal state isn't reached
         return false;
       }
     }
@@ -41,40 +36,76 @@ bool goalTest(problem currProb){
     return true;
 }
 
-void up(problem& currProb){
+bool goalTest(problem currProb){
+  for(unsigned int i = 0; i < arrSize; ++i){
+    for(unsigned int j = 0; j < arrSize; ++j){
+      if(goalState[i][j] != currProb.currState[i][j]){ //if any cell does not match then goal state isn't reached
+        return false;
+      }
+    }
+  }
+    return true;
+}
+
+problem up(problem currProb){
+  //move number below blank space up
   if(currProb.x_pos >= 0 && currProb.x_pos <= 1){
       currProb.currState[currProb.x_pos][currProb.y_pos] = currProb.currState[currProb.x_pos+1][currProb.y_pos];
       currProb.currState[currProb.x_pos+1][currProb.y_pos] = 0;
       currProb.x_pos = currProb.x_pos+1;
   }
+  currProb.g_n += 1; //parent g_n + 1
+  return currProb; //return modified problem
 }
 
-void down(problem& currProb){
+problem down(problem currProb){
   if(currProb.x_pos >= 1 && currProb.x_pos <= 2){
       currProb.currState[currProb.x_pos][currProb.y_pos] = currProb.currState[currProb.x_pos-1][currProb.y_pos];
       currProb.currState[currProb.x_pos-1][currProb.y_pos] = 0;
       currProb.x_pos = currProb.x_pos-1;
   }
+  currProb.g_n += 1; //parent g+n + 1
+  return currProb; //return modified problem
 }
 
-void right(problem& currProb){
+problem right(problem currProb){
   if(currProb.y_pos >= 1 && currProb.y_pos <= 2){
       currProb.currState[currProb.x_pos][currProb.y_pos] = currProb.currState[currProb.x_pos][currProb.y_pos-1];
       currProb.currState[currProb.x_pos][currProb.y_pos-1] = 0;
       currProb.y_pos = currProb.y_pos-1;
   }
+  currProb.g_n += 1; //parent g+n + 1
+  return currProb; //return modified problem
 }
 
-void left(problem& currProb){
+problem left(problem currProb){
   if(currProb.y_pos >= 0 && currProb.y_pos <= 1){
       currProb.currState[currProb.x_pos][currProb.y_pos] = currProb.currState[currProb.x_pos][currProb.y_pos+1];
       currProb.currState[currProb.x_pos][currProb.y_pos+1] = 0;
       currProb.y_pos = currProb.y_pos+1;
   }
+  currProb.g_n += 1; //parent g+n + 1
+  return currProb; //return modified problem
 }
 
-//data structure to track nodes
-queue<problem> nodes;
+void expand(problem currProb, queue<problem>& nodes){
+  problem upOperator = up(currProb);
+  if(!equalProblems(currProb, upOperator)){
+    nodes.push(upOperator);
+  }
+  problem downOperator = down(currProb);
+  if(!equalProblems(currProb, downOperator)){
+    nodes.push(downOperator);
+  }
+  problem leftOperator = left(currProb);
+  if(!equalProblems(currProb, leftOperator)){
+    nodes.push(leftOperator);
+  }
+  problem rightOperator = right(currProb);
+  if(!equalProblems(currProb, rightOperator)){
+    nodes.push(rightOperator);
+  }
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
@@ -87,17 +118,27 @@ int main() {
     eight_puzzle.currState[i] = new int[3];
   }
 
-  //initialize current state to the initial state
-  for(unsigned int i = 0; i < arrSize; ++i) {
+  int initialState[3][3] = {8,3,5,4,1,0,2,7,6}; //random start state
+
+  //initialize eight-puzzle to the initial state
+  /* Initialize eigh-puzzle to the initial state
+      -Puzzle is initialized to random start State
+      -Depth cost of initial state is 0
+      -Heuristic cost for Uniform cost search is 0
+  */
+
+  for(unsigned int i = 0; i < arrSize; ++i) { //initialize puzzle to initial state
     for(unsigned int j = 0; j < arrSize; ++j) {
-      eight_puzzle.currState[i][j] = eight_puzzle.initialState[i][j];
+      eight_puzzle.currState[i][j] = initialState[i][j];
     }
   }
+  eight_puzzle.x_pos = 1; //set x-coordinate for blank spot
+  eight_puzzle.y_pos = 2; //set y-coordinate for blank spot
 
   cout << "Initial State:\n";
   for(unsigned int i = 0; i < arrSize; ++i) {
     for(unsigned int j = 0; j < arrSize; ++j) {
-      cout << eight_puzzle.initialState[i][j] << ' ';
+      cout << initialState[i][j] << ' ';
     }
     cout << "\n";
   }
@@ -110,13 +151,60 @@ int main() {
     cout << "\n";
   }
 
-  nodes.push(eight_puzzle.initialState);
+  //data structure to track nodes
+  queue<problem> nodes;
+  problem currProb;
 
-  if(goalTest(eight_puzzle)){
-    cout << "Solved!\n";
-  } else {
-    cout << "Not solved\n";
+  //push initial state (root of tree) into queue
+  nodes.push(eight_puzzle);
+  currProb = nodes.front();
+  nodes.pop();
+  cout << "Before:\n";
+  for(unsigned int i = 0; i < arrSize; ++i){
+    for(unsigned int j = 0; j < arrSize; ++j){
+      cout << currProb.currState[i][j] << ' ';
+    }
+    cout << "\n";
   }
+
+  expand(currProb, nodes);
+  if(!nodes.empty()){
+    currProb = nodes.front();
+    nodes.pop();
+  }
+  cout << "After 1:\n";
+  for(unsigned int i = 0; i < arrSize; ++i){
+    for(unsigned int j = 0; j < arrSize; ++j){
+      cout << currProb.currState[i][j] << ' ';
+    }
+    cout << "\n";
+  }
+
+  if(!nodes.empty()){
+    currProb = nodes.front();
+    nodes.pop();
+  }
+  cout << " After 2:\n";
+  for(unsigned int i = 0; i < arrSize; ++i){
+    for(unsigned int j = 0; j < arrSize; ++j){
+      cout << currProb.currState[i][j] << ' ';
+    }
+    cout << "\n";
+  }
+
+
+  /*while(!nodes.empty()){
+    currProb = nodes.front();
+    nodes.pop();
+    if(goalTest(currProb)){
+      return 0; //success algorithm done
+    }
+    //expand - get all children nodes from currProb by using all possivle operators
+    if(nodes.empty()){
+      cout << "algorithm failed.\n";
+      return -1;
+    }
+  }*/
 
   return 0;
 }
